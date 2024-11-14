fa24c414a6fddebb4ceb190bfdc9933c31ae953a
vamegah <makvim@yahoo.com>
2024-11-14 01:10:55 -0500

    Fetch yoga data api

---
+++ src/Components/FetchYogaData.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import UseFetch from './UseFetch';
+    
+const FetchYogaData = () => {
+    const [data]=UseFetch('https://api.npoint.io/4459a9a10e43812e1152');
+    console.log(data);
+     return (
+           <>
+            <ul className='list_data_main'>
+                {data && data.map(e=>(
+                <>
+                <li className='list_data'>
+                    <h3>{e.name}</h3>
+                    <p><strong>Benefits: </strong>{e.benefits}</p>
+                    <p><strong>Time Duration: </strong>{e.time_duration}</p>
+                </li>
+            </>
+        ))}
+        </ul>
+    </>
+    )
+}
+export default FetchYogaData
